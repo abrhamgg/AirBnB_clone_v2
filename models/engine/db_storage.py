6bb41d0e012f28daece962b6caef608a262c7ecc
abrhamgg <abrhamgg@gmail.com>
2022-09-15 02:13:51 +0300

    place amenity

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -44,7 +44,7 @@ class DBStorage:
         session = self.__session
         dic = {}
         if not cls:
-            tables = [User, State, City, Place, Review]
+            tables = [User, State, City, Place, Review, Amenity]
         else:
             if type(cls) == str:
                 cls = eval(cls)
