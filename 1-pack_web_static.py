54ab5cf0e6f9dc563ef18515b32d9008b617ee5f
abrhamgg <abrhamgg@gmail.com>
2022-09-28 21:44:53 +0300

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,15 +2,12 @@
 """fab script that generates a tgz file"""
 import os.path
 import datetime
-from time import strftime
-
 from fabric.api import run, local
-
-time = strftime("%Y%M%d%H%M%S")
 
 
 def do_pack():
     """function that generates a tgz file"""
+    time = strftime("%Y%M%d%H%M%S")
     local('mkdir -p versions')
     try:
         local(f'tar -cvzf  versions/web_static_{time}.tgz web_static/')
