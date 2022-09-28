54b5a8b3b6041f9bd808ccc8680aba7a3e5336cd
abrhamgg <abrhamgg@gmail.com>
2022-09-28 21:33:03 +0300

    create archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""fabscript that generates a tgz file"""
+import os.path
+import datetime
+from time import strftime
+
+from fabric.api import run, local
+
+time = strftime("%Y%M%d%H%M%S")
+
+def do_pack():
+    """function that generates a tgz file"""
+    local('mkdir -p versions')
+    try:
+        local(f'tar -cvzf  versions/web_static_{time}.tgz web_static/')
+        return
+    except:
+        return None
