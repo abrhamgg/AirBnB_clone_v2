afbcce8c76d9fe53f23dadda7c14ee165d522499
abrhamgg <abrhamgg@gmail.com>
2022-09-29 17:28:01 +0300

    f strings

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,13 +17,14 @@ def do_deploy(archive_path):
     filename_full = filename + '.tgz'
     try:
         put(archive_path, "/tmp/")
-        run(f'tar xzvf /tmp/{filename_full} -C /data/web_static/releases/')
-        run(f'mv /data/web_static/releases/web_static '
-            f'/data/web_static/releases/{filename}')
-        run(f'rm -rf /tmp/{filename_full}')
+        run('tar xzvf /tmp/{} -C '
+            '/data/web_static/releases/'.format(filename_full))
+        run('mv /data/web_static/releases/web_static '
+            '/data/web_static/releases/{}'.format(filename))
+        run('rm -rf /tmp/{}'.format(filename_full))
         run('rm /data/web_static/current')
-        run(f'ln -s /data/web_static/releases/{filename} '
-            f'/data/web_static/current')
+        run('ln -s /data/web_static/releases/{} '
+            '/data/web_static/current'.format(filename))
         return True
     except Exception:
         return False
