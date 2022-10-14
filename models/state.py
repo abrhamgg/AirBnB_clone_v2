ce448c74da8b966f0d076d5864e9c4020151352a
abrhamgg <abrhamgg@gmail.com>
2022-10-14 22:39:58 +0300

    fix

--- models/state.py
+++ models/state.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
-from models import storage_type
-from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """ State class / table model"""
     __tablename__ = 'states'
-    __table_args__ = {'extend_existing': True}
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
-        name = ""
+        name = ''
 
         @property
         def cities(self):
-            """Getter attribute cities that returns the list of City instance
-            with state_id == State.id
-            """
-            found = []
-            cities = models.storage.all(City)
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
             for city in cities.values():
                 if city.state_id == self.id:
-                    found.append(city)
-            return found
+                    related_cities.append(city)
+            return related_cities
