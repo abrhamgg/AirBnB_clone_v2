b3d0d335219939a840e304852b1dc84c3ade9276
abrhamgg <abrhamgg@gmail.com>
2022-10-14 10:59:14 +0300

    route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Basic flask app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Displays Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def cIsFun(text):
+    """accepts a string argument as input"""
+    text = text.replace(' ', '_')
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
