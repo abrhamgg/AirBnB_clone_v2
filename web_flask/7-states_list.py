f97bf9d3629bfabf1a24bf1033d2c04fc1314a10
abrhamgg <abrhamgg@gmail.com>
2022-10-16 10:24:52 +0300

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,78 +1,29 @@
 #!/usr/bin/python3
-"""Basic flask app"""
-
-from flask import Flask, render_template
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+    
+"""
 from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """Displays Hello HBNB"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Display HBNB"""
-    return "HBNB"
-
-
-@app.route('/c/<string:text>', strict_slashes=False)
-def cIsFun(text):
-    """accepts a string argument as input"""
-    text = text.replace('_', ' ')
-    return "C {}".format(text)
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<string:text>', strict_slashes=False)
-def python_is_cool(text="is cool"):
-    """accepts string input from the url"""
-    text = text.replace('_', ' ')
-    print(text)
-    return "Python {}".format(text)
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """displays n if only n is int"""
-    if type(n) is int:
-        return "{} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def num(n):
-    """render html page"""
-    if type(n) is int:
-        return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def num_type(n):
-    """render html page"""
-    if type(n) is int:
-        return render_template('6-number_odd_or_even.html', n=n)
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def tear_down():
-    """removes current sqlalchemy session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
-
-@app.route('/states_list', strict_slashes=False)
-def state_list():
-    """list states"""
-    state = storage.all("State")
-    state_list = {}
-    for k, v in state.items():
-        k = k.split('.')[1]
-        state_list[k] = v.name
-    print(state_list)
-    sorted_state = dict(sorted(state_list.items(), key=lambda item: item[1]))
-    return render_template('7-states_list.html', state_list=sorted_state)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
